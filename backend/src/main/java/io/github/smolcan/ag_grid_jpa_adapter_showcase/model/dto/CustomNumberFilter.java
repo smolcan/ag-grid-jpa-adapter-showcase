476d907cbbe7efacaa302ac5cd497f97d5e2e852
smolcan <samuel.molcan01@gmail.com>
2025-02-08 17:47:30 +0100

    new coldefs wip

--- backend/src/main/java/io/github/smolcan/ag_grid_jpa_adapter_showcase/model/dto/CustomNumberFilter.java
+++ backend/src/main/java/io/github/smolcan/ag_grid_jpa_adapter_showcase/model/dto/CustomNumberFilter.java
@@ -1,49 +1,37 @@
 package io.github.smolcan.ag_grid_jpa_adapter_showcase.model.dto;
 
-import io.github.smolcan.aggrid.jpa.adapter.filter.simple.ColumnFilter;
+import io.github.smolcan.aggrid.jpa.adapter.filter.IFilter;
 import jakarta.persistence.criteria.*;
 
-public class CustomNumberFilter extends ColumnFilter {
-    
-    private String value;
-    
-    public CustomNumberFilter() {
-        super("customNumber");
-    }
+import java.util.Map;
+
+public class CustomNumberFilter extends IFilter<CustomNumberFilterModel, CustomNumberFilterParams> {
     
     @Override
-    public Predicate toPredicate(CriteriaBuilder cb, Root<?> root, String columnName) {
-        if (this.value == null || this.value.equalsIgnoreCase("All")) {
-            return cb.and();
-        }
-
-        Path<Integer> field = root.get(columnName);
-        if (this.value.equalsIgnoreCase("Even")) {
-            return cb.equal(cb.mod(field, 2), 0);
-        } else {
-            return cb.notEqual(cb.mod(field, 2), 0);
-        }
+    public CustomNumberFilterModel recognizeFilterModel(Map<String, Object> map) {
+        CustomNumberFilterModel customNumberFilter = new CustomNumberFilterModel();
+        customNumberFilter.setValue(map.get("value").toString());
+        return customNumberFilter;
     }
 
     @Override
-    public Predicate toPredicate(CriteriaBuilder cb, Expression<?> expression) {
-        if (this.value == null || this.value.equalsIgnoreCase("All")) {
+    public CustomNumberFilterParams getDefaultFilterParams() {
+        return null;
+    }
+
+    @Override
+    public Predicate toPredicate(CriteriaBuilder cb, Expression<?> expression, CustomNumberFilterModel customNumberFilterModel) {
+        String value = customNumberFilterModel.getValue();
+        if (value == null || value.equalsIgnoreCase("All")) {
             return cb.and();
         }
 
         Expression<Integer> integerExpression = expression.as(Integer.class);
-        if (this.value.equalsIgnoreCase("Even")) {
+        if (value.equalsIgnoreCase("Even")) {
             return cb.equal(cb.mod(integerExpression, 2), 0);
         } else {
             return cb.notEqual(cb.mod(integerExpression, 2), 0);
         }
     }
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
