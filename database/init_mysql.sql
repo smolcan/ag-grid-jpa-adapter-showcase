bafcb1d6788e4d42ed45dfeed5be88af4939a3ad
smolcan <samuel.molcan01@gmail.com>
2025-04-12 14:45:37 +0200

    added support for another db

---
+++ database/init_mysql.sql
@@ -0,0 +1,55 @@
+-- Create the `trade` table
+CREATE TABLE trade (
+    trade_id INT AUTO_INCREMENT PRIMARY KEY,
+    product VARCHAR(255),
+    portfolio VARCHAR(255),
+    book VARCHAR(255),
+    submitter_id INT,
+    submitter_deal_id INT,
+    deal_type VARCHAR(255),
+    bid_type VARCHAR(255),
+    current_value DECIMAL(15,2),
+    previous_value DECIMAL(15,2),
+    pl1 DECIMAL(15,2),
+    pl2 DECIMAL(15,2),
+    gain_dx DECIMAL(15,2),
+    sx_px DECIMAL(15,2),
+    x99_out DECIMAL(15,2),
+    batch INT,
+    birth_date DATE,
+    is_sold BOOLEAN
+);
+
+-- Insert data into the `trade` table
+DELIMITER //
+CREATE PROCEDURE generate_data()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    WHILE i <= 100000 DO
+        INSERT INTO trade (product, portfolio, book, submitter_id, submitter_deal_id, deal_type, bid_type, current_value, previous_value, pl1, pl2, gain_dx, sx_px, x99_out, batch, birth_date, is_sold)
+        VALUES (
+            CONCAT('Product ', FLOOR(1 + RAND() * 10)),
+            CONCAT('Portfolio ', FLOOR(1 + RAND() * 10)),
+            CONCAT('Book ', FLOOR(1 + RAND() * 5)),
+            i * 10, 
+            i * 20, 
+            CONCAT('Type ', FLOOR(1 + RAND() * 3)),
+            CONCAT('Bid ', FLOOR(1 + RAND() * 2)),
+            RAND() * 10000, 
+            RAND() * 10000, 
+            RAND() * 100, 
+            RAND() * 100, 
+            RAND() * 50, 
+            RAND() * 50, 
+            RAND() * 50, 
+            i % 100, 
+            DATE_SUB(CURDATE(), INTERVAL (i % 365) DAY), 
+            (i % 2) = 0
+        );
+        SET i = i + 1;
+    END WHILE;
+END //
+DELIMITER ;
+
+CALL generate_data();
+DROP PROCEDURE generate_data;
---
+++ run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# save as run.sh
+
+DB_TYPE="postgres"  # Default database type
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --db)
+      DB_TYPE="$2"
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [ "$DB_TYPE" = "postgres" ]; then
+  export DB_TYPE="postgres"
+  export DB_IMAGE="postgres:15"
+  export DB_PORT="5432"
+  export DB_CONTAINER_PORT="5432"
+  export DB_URL="jdbc:postgresql://database:5432/mydatabase"
+  export DB_DRIVER_CLASS_NAME="org.postgres.Driver"
+elif [ "$DB_TYPE" = "mysql" ]; then
+  export DB_TYPE="mysql"
+  export DB_IMAGE="mysql:8"
+  export DB_PORT="3306"
+  export DB_CONTAINER_PORT="3306"
+  export DB_URL="jdbc:mysql://database:3306/mydatabase"
+  export DB_DRIVER_CLASS_NAME="com.mysql.cj.jdbc.Driver"
+else
+  echo "Unsupported database type: $DB_TYPE"
+  echo "Supported types: postgres, mysql"
+  exit 1
+fi
+
+echo $DB_TYPE
+echo $DB_IMAGE
+echo $DB_PORT
+echo $DB_CONTAINER_PORT
+echo $DB_URL
+echo $DB_DRIVER_CLASS_NAME
+
+
+
+docker compose up -d
